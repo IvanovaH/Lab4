1491d16dc8f0f59bab1b3a8ec50eb68c95dccfdc
IvanovaH <never_again_@mail.ru>
2021-01-15 16:31:18 +0300

    Second commit

--- src/Hello/Main.java
+++ src/Hello/Main.java
@@ -3,6 +3,7 @@ package Hello;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("First Commit");
+        System.out.println("Second Commit");
     }
 }
